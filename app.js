427cb8dbd6f8ccc7a9d3957b3702a33b3c1b8b2e
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-11 17:57:56 +0300

    PATCH requests

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.get('/api/v1/tours/:id', (req, res) => {
-  const id = req.params * 1;
+  const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
   if (!tour) {
@@ -56,6 +56,22 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
 });
 
 app.listen(3000, () => {
