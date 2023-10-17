6d6a7a1ca57cb670e868842a01bde996e1400108
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-17 21:43:12 +0300

    refactor for MVC

--- server.js
+++ server.js
@@ -16,37 +16,6 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR: ', err);
-  });
-
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000....');
 });
