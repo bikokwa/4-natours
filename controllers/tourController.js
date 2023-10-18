85f3795a3d6cbb4a1def1db835f677992f0c4b8d
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-18 19:09:03 +0300

    delete documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -81,9 +81,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (res, req) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (res, req) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
