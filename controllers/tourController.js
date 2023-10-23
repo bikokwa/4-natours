4ef7de20cf0ea8ec589bd9773bf6c5bbd6ead8d1
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-23 16:55:38 +0300

    make the api better: pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,7 +14,7 @@ exports.getAllTours = async (req, res) => {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    let query = Tour.find(JSON.parse(queryObj));
+    let query = Tour.find(JSON.parse(queryStr));
 
     // Sorting
     if (req.query.sort) {
@@ -30,6 +30,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=3&limit=10, 1 - 10, page 1, 11 - 20, page 2, 21 - 30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist.');
     }
 
     // EXECUTE QUERY
