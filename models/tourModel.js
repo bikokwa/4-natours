085f49ad22e09640940044e13c31a4840ed44199
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-26 14:48:49 +0300

    aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -92,6 +92,14 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+// aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
