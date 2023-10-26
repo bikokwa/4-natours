066a4ff2ed044245a9ec355d9d0ea0e7fe77f2c8
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-26 16:37:42 +0300

    data validation: built-in validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters'],
     },
     slug: String,
     duration: {
@@ -21,6 +23,8 @@ const tourSchema = new mongoose.Schema(
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingQuantity: {
       type: Number,
@@ -29,6 +33,11 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       trim: true,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     price: {
       type: Number,
