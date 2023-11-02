d4b034ddb6e5db68a0ee083ceec72817f2232d69
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-11-03 02:49:42 +0300

    password reset functionality: reset token

--- controllers/authController.js
+++ controllers/authController.js
@@ -102,3 +102,19 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+
+  // generate a random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save();
+
+  // send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
