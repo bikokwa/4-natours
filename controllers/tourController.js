0ef9fcd5aee7bffbdb5d9db7170cb95fc03265b0
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-11-10 13:56:57 +0300

    find tours within radius

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const { query } = require('express');
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.aliasingTopTours = (req, res, next) => {
@@ -92,5 +93,29 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     data: {
       plan,
     },
+  });
+});
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng',
+        400
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    status: 'success',
   });
 });
