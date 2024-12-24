7a473ce4031e3e37e1bac6ea951f8e97152def86
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-12-24 22:39:13 +0300

    connect mongodb with the Express App

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+mongoose.connect(process.env.DATABASE).then((con) => {
+  console.log(con.connections);
+  console.log('DB connection successful!');
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
