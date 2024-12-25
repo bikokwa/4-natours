cdbf94942c541675a1d6768d3910b8d2f2ba43df
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-12-25 06:40:26 +0300

    refactor for MVC

--- server.js
+++ server.js
@@ -1,46 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-mongoose.connect(process.env.DATABASE).then((con) => {
-  console.log(con.connections);
-  console.log('DB connection successful!');
-});
+dotenv.config({ path: './config.env' });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR 💥: ', err);
-  });
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+app.listen(port, () => console.log(`App running on port ${port}...`));
