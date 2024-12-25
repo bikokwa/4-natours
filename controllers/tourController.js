814f472264256409f8ef5b1fab0801f01dbcd928
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-12-25 21:22:43 +0300

    advanced API filter

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,11 +2,16 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
 
     const tours = await query;
 
