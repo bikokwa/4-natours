3bb077aed4881b343e2ce6ae517fb6196bf74c6c
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-01 20:17:38 +0300

    duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,11 @@
 const AppError = require('./../utils/appError');
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message);
+};
+
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
@@ -45,6 +51,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
