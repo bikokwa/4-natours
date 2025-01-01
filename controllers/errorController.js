209ce145b454939476eaa9bb9052d6e24f09ba98
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-01 20:41:41 +0300

    validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,15 @@
 const AppError = require('./../utils/appError');
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
   const message = `Duplicate field value: ${value}. Please use another value!`;
-  return new AppError(message);
+  return new AppError(message, 400);
 };
 
 const handleCastErrorDB = (err) => {
@@ -52,6 +58,7 @@ module.exports = (err, req, res, next) => {
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidatorError') error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
