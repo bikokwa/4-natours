20b44526260f1af28227e1db0d9418f23ffae2e0
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-03 20:32:44 +0300

    log in users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,17 @@
-const fs = require('fs');
+const catchAsync = require('./../utils/catchAsync');
+const User = require('./../models/userModel');
 
-exports.getAllUsers = (req, res) => {};
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
 
 exports.createUser = (req, res) => {};
 
