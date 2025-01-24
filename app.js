67cff6ef280ad670f28f950b7dad49851edacf68
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-24 16:07:28 +0300

    handle POST requests

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 // express() will add a bunch of methods to app
 const app = express();
 
+// adds data send in the body to the request object
+app.use(express.json());
+
 // routing is basically how an app responds to a client request
 // app.get('/', (req, res) => {
 //   res
@@ -31,6 +34,24 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
