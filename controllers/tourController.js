d4139f342ea3ce3e9eac450511a25e96bded0808
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-25 05:17:35 +0300

    chain multiple middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,6 +16,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Tour does not have name or price. Please retry.',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -31,13 +41,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((t) => t.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
