a6dfc29946720d1a7d765a75f1ee1d239bbd90b2
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-25 05:28:56 +0300

    serve static files

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // express() will add a bunch of methods to app
 const app = express();
 
+// MIDDLEWARES
 // log request object information to console
 app.use(morgan('dev'));
-
 // adds data send in the body to the request object
 app.use(express.json());
+// serve static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   next();
