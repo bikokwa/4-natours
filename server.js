9ec4de5a43dae319fd000671d4053f0b3842281d
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-26 18:18:51 +0300

    connect database with EXpress App

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING)
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
