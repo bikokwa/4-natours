290c589498390d10a3e1066851b02fc94ba5f5bc
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-27 21:57:28 +0300

    project stage

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -167,6 +167,14 @@ exports.getMonthlyPlan = async (req, res) => {
       {
         $addFields: { month: '$_id' },
       },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
     ]);
 
     res.status(200).json({
