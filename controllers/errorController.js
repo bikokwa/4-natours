1db234bfd27c7b48413693afd078652f68512dbe
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-28 14:32:25 +0300

    errors during dev vs prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,36 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR 💥', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    // detailed error
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    // friendly error
+    sendErrorProd(err, res);
+  }
 };
