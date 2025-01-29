d5976139edac40f3275d35c75015f6bc719df005
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-29 07:57:01 +0300

    log in users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,16 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const User = require('./../models/userModel');
 
-exports.getAllUsers = catchAsync((req, res, next) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
 });
 
