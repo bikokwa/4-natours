32ba89e03ca8c43e8b350bdb420bc8be4e5c1e26
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-30 13:34:59 +0300

    set HTTP security headers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -11,25 +12,34 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 // GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+
 // log request object information to console
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP. Please try again in an hour!',
 });
 app.use('/api', limiter);
-// adds data send in the body to the request object
-app.use(express.json());
-// serve static files
+
+// reads data send in the body to the request object
+app.use(express.json({ limit: '10kb' }));
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   next();
 });
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
