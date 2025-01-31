2f175127d96c6f38fdde49800552b3ee038074c4
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-01-31 13:56:16 +0300

    virtual populate Tours and Reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -115,6 +115,13 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+// virtual populate for reviews
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
+
 // DOCUMENT MIDDLEWARE: runs before .save() and .create(). NOT insertMany()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
